bdb43176197490d04ef8066d444fcb743cfeb79b
Sara Wentworth <sara@sarawentworth.com>
2022-12-20 12:49:56 -0500

    end 03-jsx begin 04-hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Kali",
-      breed: "Siamese Mix",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Loki",
-      breed: "Tuxedo Bowtie",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Bebay",
-      breed: "Salt & Pepper Black",
-    }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Kali" animal="Cat" breed="mixed" />
+    <Pet name="Loki" animal="Cat" breed="mixed" />
+    <Pet name="Bebay" animal="Cat" breed="mixed" />
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
