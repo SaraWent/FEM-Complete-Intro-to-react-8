b11ccb848a73be7219f8619e2ae28974a81adead
Sara Wentworth <sara@sarawentworth.com>
2022-12-20 11:13:53 -0500

    end 01-no-frills-react, start 02-js-tools

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Kali"),
-        React.createElement("h2", {}, "Cat"),
-        React.createElement("h3", {}, "Mixed"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
     ]);
 };
 
@@ -12,9 +12,21 @@ const App = () => {
         {},
         [
             React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Kali",
+                breed: "Siamese Mix"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Loki",
+                breed: "Tuxedo Bowtie"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Bebay",
+                breed: "Salt & Pepper Black"
+            }),
         ]
     )
 };
