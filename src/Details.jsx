78c242cc1d35c6cdd23e1df7e2afa5abe6b6cf59
Sara Wentworth <sara@sarawentworth.com>
2023-01-06 13:45:36 -0500

    end 13-portals-and-refs begin 14-context

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,43 +1,54 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Modal from "./Modal";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
-import ErrorBoundary from "./ErrorBoundary";
 
 const Details = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
   const results = useQuery(["details", id], fetchPet);
 
   if (results.isLoading) {
     return (
-      <div className="loading-name">
-        <h2 className="loader">💥</h2>
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
       </div>
     );
   }
 
   const pet = results.data.pets[0];
+
   return (
     <div className="details">
-      <Carousel images={pet.images} />;
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
-        <h2>
-          {pet.animal} — {pet.breed} — {pet.city}, {pet.state}
-        </h2>
-        <button>Adopt {pet.name}</button>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
 };
 
-function DetailsErrorBoundary(props) {
+export default function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
       <Details {...props} />
     </ErrorBoundary>
   );
 }
-
-export default DetailsErrorBoundary;
